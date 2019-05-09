97c3fbd6897a330d9ac731747ed5f270186c101a
Jack Park <jackpark@gmail.com>
2019-05-09 08:47:55 -0700

    Added database operations to DSL

--- src/main/java/org/topicquests/krr/DSL.java
+++ src/main/java/org/topicquests/krr/DSL.java
@@ -5,7 +5,10 @@
  */
 package org.topicquests.krr;
 
+import java.io.*;
 import java.util.UUID;
+
+import org.topicquests.krr.engine.api.IJSONAtomSpace;
 
 
 /**
@@ -14,11 +17,47 @@ import java.util.UUID;
  */
 public class DSL {
 	private Environment environment;
+	private IJSONAtomSpace database;
 	/**
 	 * 
 	 */
 	public DSL(Environment env) {
 		environment = env;
+		database = environment.getInMemoryDatabase();
+	}
+	
+	/**
+	 * Save both node and link files as JSON strings
+	 * @param nodeFile
+	 * @param linkFile
+	 * @throws Exception
+	 */
+	public void saveDatabase(File nodeFile, File linkFile) throws Exception {
+		Writer out = new PrintWriter(nodeFile, "UTF-8");
+		database.saveNodeDatabase(out);
+		out.flush();
+		out.close();
+		out = new PrintWriter(linkFile, "UTF-8");
+		database.saveLinkDatabase(out);
+		out.flush();
+		out.close();
+	}
+	
+	/**
+	 * Restore both node and link database objects from JSON strings
+	 * @param nodeFile
+	 * @param linkFile
+	 * @throws Exception
+	 */
+	public void restoreDatabase(File nodeFile, File linkFile) throws Exception {
+		FileInputStream fis = new FileInputStream(nodeFile);
+		Reader rdr = new InputStreamReader(fis, "UTF-8");
+		database.restoreNodeDatabase(rdr);
+		rdr.close();
+		fis = new FileInputStream(linkFile);
+		rdr = new InputStreamReader(fis, "UTF-8");
+		database.restoreLinkDatabase(rdr);
+		rdr.close();
 	}
 	
 	//TODO build some objects
